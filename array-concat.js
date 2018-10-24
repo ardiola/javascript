a4256f8b039286647045d349985d781d5e31a2b8
ardiola <jakaardiola@gmail.com>
2018-10-24 09:16:22 +0700

    nambah file array-indexof dan array-concat

---
+++ array-concat.js
@@ -0,0 +1,12 @@
+function panggilConcat(){
+    var kota = ['jakarta','bandung','malang','surabaya','makasar']
+    var kabupaten = ['lampung timur', 'jombang', 'kediri']
+
+
+    var hasil = kota.concat(kabupaten)
+
+    return hasil
+
+}
+
+console.log(panggilConcat())
---
+++ array-indexof.js
@@ -0,0 +1,8 @@
+function panggilIndexOf() {
+    var kota = ['jakarta','bandung','malang','surabaya','makasar']
+
+    return kota.indexOf('bandung')
+}
+
+
+console.log(panggilIndexOf())
